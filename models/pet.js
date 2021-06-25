717c1701fa7c6f5758a7c06ff2cb7b7dd4fcad2e
JoselinMorales <joselin.morales.mora@ucreativa.com>
2021-06-24 21:09:10 -0600

    first commit

---
+++ models/pet.js
@@ -0,0 +1,14 @@
+class Pet{
+constructor(name, age, soecies, race, picture, description){
+this.name = name;
+this.age = age;
+this.species = soecies;
+this.race = race;
+this.picture = picture;
+this.description = description;
+}
+
+}
+
+module.exports = Pet;
+
